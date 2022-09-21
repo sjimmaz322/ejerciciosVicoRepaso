d369f4cfdab94083b45b33bce22f4f9c157f6952
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-09-21 14:41:38 +0200

    Añadidos todos los métodos en la clase Llamadas y comienzo de cración de menú en clase principal

--- src/main/java/ejercicio1/EjerciciosVicoRepaso.java
+++ src/main/java/ejercicio1/EjerciciosVicoRepaso.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package ejercicio1;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,7 +14,21 @@ package ejercicio1;
 public class EjerciciosVicoRepaso {
 
     public static void main(String[] args) {
-        //Comentario de prueba
-        System.out.println("Hello World!");
+
+        //Creamos un ArrayList para guardar las llamadas
+        ArrayList<Llamada> listadoLlamadas = new ArrayList<Llamada>();
+
+        int elección = JOptionPane.YES_NO_OPTION;
+
+        if (JOptionPane.showConfirmDialog(null, "¿Desea registrar una llamada?", "Registro Llamadas",
+                JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+            // yes option
+            JOptionPane.showMessageDialog(null, "Añadamos una llamada");
+
+        } else {
+            // no option
+            JOptionPane.showMessageDialog(null, "Entonces nada");
+        }
+
     }
 }
--- src/main/java/ejercicio1/Llamada.java
+++ src/main/java/ejercicio1/Llamada.java
@@ -1,15 +1,105 @@
 package ejercicio1;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  *
  * @author sajm
  */
 public class Llamada {
-    
+
     private String id, numeroOrigen, numeroDestino;
     private LocalDateTime fechaHoraInicio, fechaHoraFin;
-    
-    
+
+    public Llamada() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumeroOrigen() {
+        return numeroOrigen;
+    }
+
+    public void setNumeroOrigen(String numeroOrigen) {
+        this.numeroOrigen = numeroOrigen;
+    }
+
+    public String getNumeroDestino() {
+        return numeroDestino;
+    }
+
+    public void setNumeroDestino(String numeroDestino) {
+        this.numeroDestino = numeroDestino;
+    }
+
+    public LocalDateTime getFechaHoraInicio() {
+        return fechaHoraInicio;
+    }
+
+    public void setFechaHoraInicio(LocalDateTime fechaHoraInicio) {
+        this.fechaHoraInicio = fechaHoraInicio;
+    }
+
+    public LocalDateTime getFechaHoraFin() {
+        return fechaHoraFin;
+    }
+
+    public void setFechaHoraFin(LocalDateTime fechaHoraFin) {
+        this.fechaHoraFin = fechaHoraFin;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.id);
+        hash = 83 * hash + Objects.hashCode(this.numeroOrigen);
+        hash = 83 * hash + Objects.hashCode(this.numeroDestino);
+        hash = 83 * hash + Objects.hashCode(this.fechaHoraInicio);
+        hash = 83 * hash + Objects.hashCode(this.fechaHoraFin);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Llamada other = (Llamada) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.numeroOrigen, other.numeroOrigen)) {
+            return false;
+        }
+        if (!Objects.equals(this.numeroDestino, other.numeroDestino)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaHoraInicio, other.fechaHoraInicio)) {
+            return false;
+        }
+        return Objects.equals(this.fechaHoraFin, other.fechaHoraFin);
+    }
+
+    @Override
+    public String toString() {
+        return "Llamada con id: " + id
+                + "\nCon número de origen: " + numeroOrigen
+                + "\nCon número de destido: " + numeroDestino
+                + "\nIniciada en: " + fechaHoraInicio
+                + "\nFinalizada en :" + fechaHoraFin;
+    }
+
 }
