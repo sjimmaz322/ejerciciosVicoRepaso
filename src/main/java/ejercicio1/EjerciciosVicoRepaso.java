13f706585136395f0f1d2e50d916270e07cff991
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-09-21 14:24:36 +0200

    Creadas clases

---
+++ src/main/java/ejercicio1/EjerciciosVicoRepaso.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicio1;
+
+/**
+ *
+ * @author sajm
+ */
+public class EjerciciosVicoRepaso {
+
+    public static void main(String[] args) {
+        //Comentario de prueba
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ejercicio1/Llamada.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author sajm
+ */
+public class Llamada {
+    
+    private int id, numeroOrigen, numeroDestino;
+    private LocalDateTime fechaHoraInicio, fechaHoraFin;
+    
+    
+}
