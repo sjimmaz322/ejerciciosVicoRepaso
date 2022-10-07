60c88b79fcb9e439e41070004606796d9b0e5dd2
SamueljmDevs <samueljmdevs@gmail.com>
2022-10-08 00:25:13 +0200

    Creada clase Utilidades

---
+++ src/main/java/utilidades/Utilidades.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package utilidades;
+
+/**
+ *
+ * @author sajm <sjimmaz322 at sjimmaz322@g.educaand.es>
+ */
+public class Utilidades {
+
+    public static int stringToInt(String s) {//Método para pasar de String a int
+        int num = Integer.parseInt(s);
+        return num;
+    }
+
+    public static double stringToDouble(String s) {//Método para pasar de String a Double
+        double num = Double.parseDouble(s);
+        return num;
+    }
+}
