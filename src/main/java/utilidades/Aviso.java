71763502a778a43b62b5df82cbe6fdef49fd1dfa
SamueljmDevs <samueljmdevs@gmail.com>
2022-11-15 21:15:17 +0100

    Añadido meme

---
+++ src/main/java/utilidades/Aviso.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilidades;
+
+/**
+ *
+ * @author sajm <sjimmaz322 at sjimmaz322@g.educaand.es>
+ */
+public class Aviso {
+    //Querido colega programador.
+    //
+    //Cuando escribí este código, sólo Dios y yo
+    //sabíamos como funcionaba.
+    //Ahora, ¡Sólo Dios lo sabe!
+    //
+    //Así que si estás intentando "optimizarlo" o "entenderlo"
+    //y fracasas (seguramente ocurra), por favor,
+    //incrementa los contadores a continuación
+    //como una advertencia para su siguiente colega.
+    //
+    //total_horas_perdidas_aquí = 1.5;
+    //personas_que_fracasamos_con_el_código = 1;
+}
